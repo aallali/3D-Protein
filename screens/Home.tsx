8c062be32fd9c6dd7d7604f74492c397adbcc57b
3dida <44850191+aallali@users.noreply.github.com>
2024-02-08 20:03:59 +0100

    [update] : navigation buttons : @ Home

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
+import { ReactPropTypes } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NavigateButtons from '../components/NavigateButtons';
 
-export default function Home() {
-  return (
-    <View style={styles.container}>
-      <Text>Home Page</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function Home(props: any) {
+    console.log(props)
+    return (
+        <View style={styles.container}>
+            <Text>Home Page</Text>
+            <NavigateButtons go={props.navigation.navigate} t={"home"}/>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
