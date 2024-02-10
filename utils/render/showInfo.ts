75fd093f2f1d2589f1b96ddf9d59adcdd83158ef
MoldyLunchBox <duu116@gmail.com>
2024-02-10 18:57:59 +0100

    [lint]

--- utils/render/showInfo.ts
+++ utils/render/showInfo.ts
@@ -10,20 +10,16 @@ export default  function onAtomClick(event:any, camera:any, scene: any) {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
-    // Perform raycasting
+    // raycasting with clicked point
     const raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(mouse, camera);
     // Define an array to store intersected objects
-    console.log('mouse click')
     const intersects = raycaster.intersectObjects(scene.children, true);
     // Check if any objects were intersected
     if (intersects.length > 0) {
-        // Get the first intersected object (closest to the camera)
+        //first intersected object
         const clickedObject = intersects[0].object;
-
-        // Check if the clicked object is a sphere
         if (clickedObject.name) {
-            // Handle the click on the sphere
             const info = colors[clickedObject.name]
             console.log('atom clicked!', info);
         }
