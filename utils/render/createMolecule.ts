4daee4bf8cddf7e6bd14af7463792af0ecf2f405
3dida <44850191+aallali@users.noreply.github.com>
2024-02-11 23:10:29 +0100

    [fix] : incompatibility between atoms and connectors length : @utils/render/createMolecule

--- utils/render/createMolecule.ts
+++ utils/render/createMolecule.ts
@@ -39,22 +39,46 @@ export default function createMolecule(atoms: TAtoms, connectors: TConnectors) {
         molecule.add(atom);
     })
 
-    connectors.forEach(c => {
-        let atomAPos = new THREE.Vector3(
-            atoms[c[0] - 1].x,
-            atoms[c[0] - 1].y,
-            atoms[c[0] - 1].z
-        )
-        for (let i = 1; i < c.length; i++) {
-            let atomBPos = new THREE.Vector3(
-                atoms[c[i] - 1].x,
-                atoms[c[i] - 1].y,
-                atoms[c[i] - 1].z
+
+    // ISSUE:
+    // a connector is represented as array of numbers 
+    // each number represent index of atom
+    // the first index represent the atom that making connection with other
+    // [1 2 4] : atom#1 connected with Atom@2 && Atom#4
+    // sometimes the list contain an index out of range of original list of atoms
+    // we have.
+    // eg: we have 23 atoms, and one of the connectors is [1 2 25]
+    // so atom#1 trying to connect with Atm#2 && Atm#25, where the latter doesnt 
+    // exists.
+
+    // SOLUTION:
+    // check if the the Atom(connector) exists
+    // then loop through atoms(target) and create connection with valid ones
+
+    connectors.forEach((c, i) => {
+        const atom = atoms[c[0] - 1]
+        if (atom) {
+            let atomAPos = new THREE.Vector3(
+                atoms[c[0] - 1].x,
+                atoms[c[0] - 1].y,
+                atoms[c[0] - 1].z
             )
-            const connector = createConnector(atomAPos, atomBPos, 'white')
-            connector.position.sub(groupCenter);
-            molecule.add(connector)
+            for (let i = 1; i < c.length; i++) {
+                const atom = atoms[c[i] - 1]
+                if (atom) {
+                    let atomBPos = new THREE.Vector3(
+                        atom.x,
+                        atom.y,
+                        atom.z
+                    )
+
+                    const connector = createConnector(atomAPos, atomBPos, 'white')
+                    connector.position.sub(groupCenter);
+                    molecule.add(connector)
+                }
+            }
         }
     })
+
     return molecule
 }
