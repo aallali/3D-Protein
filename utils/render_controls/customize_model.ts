f06f3b76be888b332484dcbf059703215a69c3d2
3dida <44850191+aallali@users.noreply.github.com>
2024-02-11 13:07:46 +0100

    [+] : view controll utils (incomplete) : @utils/render_controls/*

---
+++ utils/render_controls/customize_model.ts
@@ -0,0 +1,23 @@
+
+export function update_cylinder_color() {
+    alert("Method not implemented!")
+}
+
+
+export function update_cylinder_shape() {
+    alert("Method not implemented!")
+}
+
+
+export function update_atom_color() {
+    alert("Method not implemented!")
+}
+
+
+export function update_atom_shape() {
+    alert("Method not implemented!")
+}
+
+export function unify_color() {
+    alert("Method not implemented!")
+}
---
+++ utils/render_controls/screenshot.ts
@@ -0,0 +1,4 @@
+
+export default function screenshotMolecule() {
+    alert("Method not implemented!")
+}
