72e1dfd8a38034d6b598562a99b55b3f16abdf74
3dida <44850191+aallali@users.noreply.github.com>
2024-02-12 22:18:47 +0100

    [lint]

--- components/AtomInfo.tsx
+++ components/AtomInfo.tsx
@@ -1,21 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import atomInfo from '../constants/colors.json';
 
-interface Props{
+interface Props {
     atom: string
 }
 
-const InfoBox = ({ atom }:Props) => {
+const InfoBox = ({ atom }: Props) => {
     const info = atomInfo[atom as keyof typeof atomInfo]
 
     return (
-    <View style={styles.infoBox}>
-        <Text>Atom: {atom}</Text>
-        <Text>discovered by: {info.discoverd_by}</Text>
-        <Text>phase: {info.phase}</Text>
-    </View>
-);
-    }
+        <View style={styles.infoBox}>
+            <Text>Atom: {atom}</Text>
+            <Text>discovered by: {info.discoverd_by}</Text>
+            <Text>phase: {info.phase}</Text>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
     infoBox: {
@@ -29,6 +29,5 @@ const styles = StyleSheet.create({
         borderColor: 'gray',
     },
 });
-
 
 export default InfoBox
