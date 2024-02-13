da35c0b54738aba5208866da6a285438a9d58f65
3dida <44850191+aallali@users.noreply.github.com>
2024-02-13 14:19:18 +0100

    [fix] : normalize atom elem symbole : @utils/atomSymboleNormalizer.ts

--- components/AtomInfo.tsx
+++ components/AtomInfo.tsx
@@ -1,16 +1,20 @@
 import { StyleSheet, Text, View } from "react-native";
 import atomInfo from '../constants/colors.json';
 import atomName from '../constants/atoms.json'
+import { normalizeAtomSymbole } from "../utils/atomSymboleNormalizer";
+
+
 interface Props {
     atom: string
 }
 
 const InfoBox = ({ atom }: Props) => {
-    const info = atomInfo[atom as keyof typeof atomInfo]
+    const atomSymbole = normalizeAtomSymbole(atom) as keyof typeof atomInfo
+    const info = atomInfo[atomSymbole]
 
     return (
         <View style={styles.infoBox}>
-            <Text>Atom: ( {atom} ) {atomName[atom as keyof typeof atomInfo]}</Text>
+            <Text>Atom: ( {atomSymbole} ) {atomName[atomSymbole]}</Text>
             <Text>discovered by: {info.discoverd_by}</Text>
             <Text>phase: {info.phase}</Text>
         </View>
